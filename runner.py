56aed3486819a275110ea8a665f1dda38bb22155
openhaijaz <openhaijaz@gmail.com>
2014-02-07 22:28:26 -0600

    maininized

---
+++ runner.py
@@ -0,0 +1,6 @@
+import daemon
+from walscraper import main
+
+print daemon.__file__
+# with daemon.DaemonContext():
+    # main()
--- walscraper.py
+++ walscraper.py
@@ -3,32 +3,38 @@ import requests
 from BeautifulSoup import BeautifulSoup
 import time
 import random
-nogood=1
-x=0
-while nogood==1:
-	time.sleep(random.randint(120, 420))
-	r = requests.get('http://goo.gl/GdxkKC')
-	soup=BeautifulSoup(r.text)
-	y = soup.find(id='shelfDiv').find(id="border")
-	new = str(y)
-	newfile=open('new.txt', 'w+')
-	newfile.write (new)
-	newfile.close()
-	reopen=open('new.txt', 'w+')
-	new=reopen.read()
-	reopen.close()
-	f=open('baseline.txt', 'w+')
-	old=f.read()
-	if new==old:
-		print "round:  " + str(x)
-		x+=1
-	else:
-		server = smtplib.SMTP("smtp.gmail.com", 587)
-		server.starttls()
-		server.login('openhaijaz@gmail.com', 'ektelo9n')
-		server.sendmail("me!!", "7032258785@messaging.sprintpcs.com", "http://goo.gl/GdxkKC")
-		print 'noooooooooo'
-		f.write(new)
-		nogood=0
-		break
-	f.close()
+
+def main():
+	nogood=1
+	x=0
+	while nogood==1:
+		bitly = "http://goo.gl/WNOecx"
+		time.sleep(random.randint(12, 15))
+		r = requests.get(bitly)
+		soup=BeautifulSoup(r.text)
+		y = soup.find(id='shelfDiv').find(id="border")
+		new = str(y)
+		newfile=open('new.txt', 'w+')
+		newfile.write (new)
+		newfile.close()
+		reopen=open('new.txt', 'w+')
+		new=reopen.read()
+		reopen.close()
+		f=open('baseline.txt', 'w+')
+		old=f.read()
+		if new==old:
+			print "round:  " + str(x)
+			x+=1
+		else:
+			server = smtplib.SMTP("smtp.gmail.com", 587)
+			server.starttls()
+			server.login('openhaijaz@gmail.com', 'ektelo9n')
+			server.sendmail("me!!", "7032258785@messaging.sprintpcs.com", bitly)
+			print 'noooooooooo'
+			f.write(new)
+			nogood=0
+			break
+		f.close()
+		
+if __name__ == "__main__":
+    main()
